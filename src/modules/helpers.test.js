a80ce235233d9d9df25c7d52f25538258316c81e
eryner-z <stfn.bgr@gmail.com>
2022-03-01 18:10:26 +0100

    add helper function test

--- src/modules/helpers.test.js
+++ src/modules/helpers.test.js
@@ -1,9 +1,17 @@
-const getCoordinates = require('./helpers');
-const shipFactory = require('./ship');
-const { gameboardFactory, gameboard, myFleet } = require('./gameboard');
+import { myFleet, gameboardFactory, gameboard } from './gameboard';
+import { getCoordinates } from './helpers';
+import { shipFactory } from './ship';
 
-test('1 correct ship coordinates get returned (hor)', () => {
-  gameboardFactory();
+beforeEach(() => {
+  const gameboardMock = jest.fn();
+  gameboardMock.mockReturnValue(gameboardFactory());
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+test('correct ship coordinates get returned (hor)', () => {
   expect(getCoordinates('carrier', 'horizontal', [0, 5])).toEqual([
     [0, 5],
     [0, 6],
@@ -13,47 +21,21 @@ test('1 correct ship coordinates get returned (hor)', () => {
   ]);
 });
 
-/* test('2 correct ship coordinates get returned (hor)', () => {
-  const board = gameboardFactory();
-  expect(
-    board.placeShip(
-      'carrier',
-      5,
-      getCoordinates('carrier', 'horizontal', [0, 5])
-    )
-  ).toEqual([
-    [0, 5],
-    [0, 6],
-    [0, 7],
-    [0, 8],
-    [0, 9],
-  ]);
-}); */
-
-/* test('invalid positions are rejected', () => {
-  expect(gameboardFactory().gameboard).toEqual([null]);
-}); */
-
-/* test('correct ship coordinates get returned (hor)', () => {
-  expect(
-    getCoordinates('carrier', 'horizontal', [0, 5]).validCoordinates
-  ).toEqual([
-    [0, 5],
-    [0, 6],
-    [0, 7],
-    [0, 8],
-    [0, 9],
-  ]);
-}); */
-
-/* test('correct ship coordinates get returned (vert)', () => {
-  expect(
-    getCoordinates('carrier', 'vertical', [0, 5]).validCoordinates
-  ).toEqual([
+test('correct ship coordinates get returned (vert)', () => {
+  expect(getCoordinates('carrier', 'vertical', [0, 5])).toEqual([
     [0, 5],
     [1, 5],
     [2, 5],
     [3, 5],
     [4, 5],
   ]);
-}); */
+});
+
+test('invalid positions are rejected (outside the board)', () => {
+  expect(getCoordinates('carrier', 'horizontal', [0, 8])).toEqual([null]);
+});
+
+test('invalid positions are rejected (field is already occupied)', () => {
+  gameboard.illegalPositions.push([0, 5], [0, 6], [0, 7], [0, 8], [0, 9]);
+  expect(getCoordinates('carrier', 'horizontal', [0, 5])).toEqual([null]);
+});
