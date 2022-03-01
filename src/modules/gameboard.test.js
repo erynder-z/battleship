029adc00b7110c81967ed393ab655530b1b92864
eryner-z <stfn.bgr@gmail.com>
2022-03-01 20:51:19 +0100

    ship placement test

--- src/modules/gameboard.test.js
+++ src/modules/gameboard.test.js
@@ -1,16 +1,34 @@
-const gameboardFactory = require('./gameboard');
+import { gameboardFactory } from './gameboard';
 
-const mockGameboardEmpty = () => gameboardFactory();
+const board = gameboardFactory();
 
 test('grid fields are correctly named', () => {
-  expect(mockGameboardEmpty()[0][1].horizontal).toEqual(1);
+  expect(board[0][1].horizontal).toEqual(1);
 });
 
 test('grid fields are objects with correct key-values', () => {
-  expect(mockGameboardEmpty()[3][4]).toEqual({
+  expect(board[3][4]).toEqual({
     hit: false,
     occupied: false,
     vertical: 3,
     horizontal: 4,
   });
+});
+
+test('ships can be placed', () => {
+  board.placeShip('carrier', 5, [
+    [0, 5],
+    [0, 6],
+    [0, 7],
+    [0, 8],
+    [0, 9],
+  ]);
+  expect(
+    board[0][5].occupied &&
+      board[0][6].occupied &&
+      board[0][7].occupied &&
+      board[0][8].occupied &&
+      board[0][9].occupied
+  ).toBe(true);
+  expect(board[0][4].occupied).toBe(false);
 });
